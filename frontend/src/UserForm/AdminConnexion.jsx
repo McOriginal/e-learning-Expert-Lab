fe5d58bcdd37647b1eee53db697677bd95c75d41
McOriginal <originalmc223@gmail.com>
2024-12-09 12:48:16 +0000

    Correction front et back end admin cours

--- frontend/src/UserForm/AdminConnexion.jsx
+++ frontend/src/UserForm/AdminConnexion.jsx
@@ -35,7 +35,7 @@ export default function AdminConnexion({ setIsAuthenticated }) {
                 setMessage('Connexion réussie !');
                 setTimeout(() => {
                     setIsAuthenticated(true);
-                    navigate('https://e-learning-expert-lab-frontend.onrender.com/admin/admincours');
+                    navigate('http://e-learning-expert-lab-frontend.onrender.com/admin/admincours');
                 }, 3000);
             } else {
                 setTimeout(() => {
